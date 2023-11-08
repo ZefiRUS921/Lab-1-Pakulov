3004502894962249a82b186e732d98fd0260afcc
ZefiRUS921 <150244705+ZefiRUS921@users.noreply.github.com>
2023-11-08 17:37:32 +0300

    Add files via upload

---
+++ Лаба 1.2 Пакулов/Лаба 1.2 Пакулов/Students.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+/// <summary>
+/// Базовый класс для хранения информации о студентах
+/// </summary>
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+/// <summary>
+/// Класс для хранения информации о старосте группы
+/// </summary>
+class HeadStudent : Student
+{
+    public string Responsibilities { get; set; }
+}
+
+/// <summary>
+/// Класс для хранения информации о группе студентов
+/// </summary>
+class Group
+{
+    public string Name { get; set; }
+    public HeadStudent Head { get; set; }
+    public Student[] Students { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Группа: {Name}");
+        Console.WriteLine($"Староста группы: {Head.Name}, Обязанности: {Head.Responsibilities}");
+        Console.WriteLine("Студенты:");
+        foreach (var student in Students)
+        {
+            Console.WriteLine($"{student.Name}, Возраст: {student.Age}");
+        }
+    }
+}
+
+/// <summary>
+/// Класс для режима отображения информации
+/// </summary>
+class ReadOnlyMode
+{
+    public void Display(Group group)
+    {
+        group.DisplayInfo();
+    }
+}
+
+/// <summary>
+/// Класс для режима редактирования информации
+/// </summary>
+class EditMode : ReadOnlyMode
+{
+    public void EditHeadResponsibilities(Group group, string responsibilities)
+    {
+        group.Head.Responsibilities = responsibilities;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student[] students = new Student[]
+        {
+            new Student { Name = "Алекс", Age = 20 },
+            new Student { Name = "Лена", Age = 21 },
+            new Student { Name = "Иван", Age = 19 }
+        };
+
+        Group group = new Group
+        {
+            Name = "Группа А",
+            Head = new HeadStudent { Name = "Виктор", Age = 22, Responsibilities = "Координатор группы" },
+            Students = students
+        };
+
+        ReadOnlyMode readOnlyMode = new ReadOnlyMode();
+        readOnlyMode.Display(group);
+
+        EditMode editMode = new EditMode();
+        editMode.EditHeadResponsibilities(group, "Староста группы");
+
+        Console.WriteLine("После редактирования:");
+        readOnlyMode.Display(group);
+    }
+}
---
+++ Лаба 1.3 Пакулов/Лаба 1.3 Пакулов Ю/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+/// <summary>
+/// Класс для представления события в логе
+/// </summary>
+class Event
+{
+    public string Date { get; set; } // Дата и время события
+    public string Result { get; set; } // Результат события (например, успех или ошибка)
+    public string IpFrom { get; set; } // IP-адрес отправителя
+    public string Method { get; set; } // HTTP-метод (GET, POST и т.д.)
+    public string UrlTo { get; set; } // URL, куда был отправлен запрос
+    public string Response { get; set; } // HTTP-код ответа
+}
+
+/// <summary>
+/// Класс для представления лога событий
+/// </summary>
+class Log
+{
+    public List<Event> Events { get; set; } // Список событий в логе
+
+    public Log()
+    {
+        Events = new List<Event>();
+    }
+}
+
+/// <summary>
+/// Класс для парсинга XML и создания объектов лога
+/// </summary>
+class XmlParser
+{
+    public static Log ParseXml(string xmlString)
+    {
+        // Создание объекта для хранения событий лога
+        Log log = new Log();
+
+        // Загрузка XML
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlString);
+
+        // Получение всех элементов <event>
+        XmlNodeList eventNodes = doc.DocumentElement.SelectNodes("event");
+
+        // Обход всех <event> элементов
+        foreach (XmlNode eventNode in eventNodes)
+        {
+            // Создание объекта Event на основе данных из XML
+            Event e = new Event();
+            e.Date = eventNode.Attributes["date"].Value;
+            e.Result = eventNode.Attributes["result"].Value;
+            e.IpFrom = eventNode.SelectSingleNode("ip-from").InnerText;
+            e.Method = eventNode.SelectSingleNode("method").InnerText;
+            e.UrlTo = eventNode.SelectSingleNode("url-to").InnerText;
+            e.Response = eventNode.SelectSingleNode("response").InnerText;
+
+            // Добавление события в список событий лога
+            log.Events.Add(e);
+        }
+
+        return log; // Возврат заполненного лога
+    }
+}
+
+
+/// <summary>
+/// Пример использования XmlParser
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+        // Пример XML-строки
+        string xml = @"<log>
+                          <event date=""2022-03-27 13:45:30"" result=""success"">
+                            <ip-from>192.168.1.1</ip-from>
+                            <method>GET</method>
+                            <url-to>http://example.com</url-to>
+                            <response>200</response>
+                          </event>
+                          <event date=""2022-03-27 14:15:20"" result=""error"">
+                            <ip-from>192.168.1.2</ip-from>
+                            <method>POST</method>
+                            <url-to>http://example.com/api</url-to>
+                            <response>404</response>
+                          </event>
+                        </log>";
+
+        // Парсинг XML и создание объекта лога
+        Log log = XmlParser.ParseXml(xml);
+
+        // Вывод данных каждого события
+        foreach (var e in log.Events)
+        {
+            Console.WriteLine($"Date: {e.Date}, Result: {e.Result}, IP: {e.IpFrom}, Method: {e.Method}, URL: {e.UrlTo}, Response: {e.Response}");
+        }
+    }
+}
